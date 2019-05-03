d183fcf871f8a33347f4e3c07c32b8f7185401fb
Skullbonk <reagan.fairbanks@gmail.com>
2019-05-03 13:54:42 -0600

    Added panels

---
+++ src/draw/view/DrawAppPanel.java
@@ -0,0 +1,41 @@
+package draw.view;
+
+import javax.swing.*;
+import draw.controller.DrawController;
+
+public class DrawAppPanel
+{
+	private DrawController controller;
+
+	public DrawAppPanel(DrawController controller)
+	{
+		super();
+		this.controller = controller;
+	}
+	
+	private void setupMenuPanels()
+	{
+		
+	}
+	
+	private void setupScrollPane()
+	{
+		
+	}
+	
+	private void setupPanel()
+	{
+
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
+
+}
---
+++ src/draw/view/DrawPanel.java
@@ -0,0 +1,16 @@
+package draw.view;
+
+import javax.swing.JPanel;
+import draw.controller.DrawController;
+
+public class DrawPanel
+{
+	private DrawController controller;
+	
+	public DrawPanel(DrawController controller)
+	{
+		super();
+		this.controller = controller;
+	}
+
+}
